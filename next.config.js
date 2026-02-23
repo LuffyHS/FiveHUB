8922e36250e388ed6210e3e699f1d9d073e5f66c
LuffyH$ <105009086+LuffyHS@users.noreply.github.com>
2026-02-22 21:51:36 -0300

    Update 2.7.1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // IMPORTANT: do NOT use `output: 'export'` on Vercel for this project.
+  // IMPORTANT: Do NOT use `output: 'export'` on Vercel for this project.
   // It causes prerender/export errors with dynamic data (VLR APIs).
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" }
+      { protocol: "http", hostname: "**" },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
